e25917dd223690f917063878441c33d121d695b8
Nicolas Suzuki <5541611+nicolas-suzuki@users.noreply.github.com>
2020-09-21 17:47:19 -0300

    Base

--- app/src/main/java/com/aden/radq/MainActivity.java
+++ app/src/main/java/com/aden/radq/MainActivity.java
@@ -15,7 +15,6 @@ import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
 
-import org.opencv.*;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfFloat;
@@ -31,7 +30,6 @@ import org.opencv.imgproc.Imgproc;
 import org.opencv.utils.Converters;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
@@ -39,6 +37,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     BaseLoaderCallback baseLoaderCallback;
     boolean startYolo = false;
     boolean firstTimeYolo = false;
+    int framesParaConfirmarQueda = 0;
     Net tinyYolo;
 
     public void YOLO(View Button){
@@ -47,10 +46,10 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
             if (firstTimeYolo == false){
                 firstTimeYolo = true;
-                String tinyYoloCfg = Environment.getExternalStorageDirectory() + "/dnns/yolov3-tiny.cfg" ;
-                String tinyYoloWeights = Environment.getExternalStorageDirectory() + "/dnns/yolov3-tiny.weights";
+                String tinyYoloCfg = getExternalFilesDir(null) + "/dnns/yolov3-tiny.cfg" ;
+                String tinyYoloWeights = getExternalFilesDir(null) + "/dnns/yolov3-tiny.weights";
 
-                Log.i("tinyLocation" , "Tiny Weights: " + tinyYoloWeights + "\nTiny CFG: " + tinyYoloCfg);
+                Log.i("tinyLocation1" , "Tiny Weights: " + tinyYoloWeights + "\nTiny CFG: " + tinyYoloCfg);
                 tinyYolo = Dnn.readNetFromDarknet(tinyYoloCfg, tinyYoloWeights);
             }
         }
@@ -63,7 +62,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         cameraBridgeViewBase = (JavaCameraView)findViewById(R.id.CameraView);
         cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
@@ -132,6 +131,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
             }
             int ArrayLength = confs.size();
 
+            //deteccao
             if (ArrayLength >= 1) {
                 // Apply non-maximum suppression procedure.
                 float nmsThresh = 0.2f;
@@ -148,10 +148,25 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
                     Rect box = boxesArray[idx];
                     int idGuy = clsIds.get(idx);
                     float conf = confs.get(idx);
-                    List<String> cocoNames = Arrays.asList("a person", "a bicycle", "a motorbike", "an airplane", "a bus", "a train", "a truck", "a boat", "a traffic light", "a fire hydrant", "a stop sign", "a parking meter", "a car", "a bench", "a bird", "a cat", "a dog", "a horse", "a sheep", "a cow", "an elephant", "a bear", "a zebra", "a giraffe", "a backpack", "an umbrella", "a handbag", "a tie", "a suitcase", "a frisbee", "skis", "a snowboard", "a sports ball", "a kite", "a baseball bat", "a baseball glove", "a skateboard", "a surfboard", "a tennis racket", "a bottle", "a wine glass", "a cup", "a fork", "a knife", "a spoon", "a bowl", "a banana", "an apple", "a sandwich", "an orange", "broccoli", "a carrot", "a hot dog", "a pizza", "a doughnut", "a cake", "a chair", "a sofa", "a potted plant", "a bed", "a dining table", "a toilet", "a TV monitor", "a laptop", "a computer mouse", "a remote control", "a keyboard", "a cell phone", "a microwave", "an oven", "a toaster", "a sink", "a refrigerator", "a book", "a clock", "a vase", "a pair of scissors", "a teddy bear", "a hair drier", "a toothbrush");
                     int intConf = (int) (conf * 100);
-                    Imgproc.putText(frame, cocoNames.get(idGuy) + " " + intConf + "%", box.tl(), Core.FONT_HERSHEY_SIMPLEX, 2, new Scalar(255, 255, 0), 2);
-                    Imgproc.rectangle(frame, box.tl(), box.br(), new Scalar(255, 0, 0), 2);
+
+                    if(idGuy==0){
+                        // Queda detectada
+                        Imgproc.putText(frame, "Queda Detectada" + " " + intConf + "%", box.tl(), Core.FONT_HERSHEY_SIMPLEX, 2, new Scalar(255, 255, 0), 2);
+                        Imgproc.rectangle(frame, box.tl(), box.br(), new Scalar(255, 0, 0), 5);
+                        Log.i("deteccao","Queda Detectada! Precisão: " + intConf + "%");
+                        framesParaConfirmarQueda++;
+                        if (framesParaConfirmarQueda > 10) {
+                            framesParaConfirmarQueda = 0;
+                            Log.i("deteccao", "Queda Confirmada");
+                        }
+                    } else if (idGuy==1){
+                        // Pessoa detectada
+                        Imgproc.putText(frame, "deteccao" + " " + intConf + "%", box.tl(), Core.FONT_HERSHEY_SIMPLEX, 2, new Scalar(255, 255, 0), 2);
+                        Imgproc.rectangle(frame, box.tl(), box.br(), new Scalar(0, 255, 0), 2);
+                    } else {
+                        Log.w("deteccao","idGuy!=0||1");
+                    }
                 }
             }
         }
@@ -161,9 +176,9 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     @Override
     public void onCameraViewStarted(int width, int height) {
         if (startYolo == true){
-            String tinyYoloCfg = Environment.getExternalStorageDirectory() + "/dnns/yolov-obj.cfg" ;
-            String tinyYoloWeights = Environment.getExternalStorageDirectory() + "/dnns/yolov-obj.weights";
-            Log.i("tinyLocation" , "Tiny Weights: " + tinyYoloWeights + "\nTiny CFG: " + tinyYoloCfg);
+            String tinyYoloCfg = Environment.getExternalStorageDirectory() + "/dnns/yolov3-tiny.cfg" ;
+            String tinyYoloWeights = Environment.getExternalStorageDirectory() + "/dnns/yolov3-tiny.weights";
+            Log.i("tinyLocation2" , "Tiny Weights: " + tinyYoloWeights + "\nTiny CFG: " + tinyYoloCfg);
             tinyYolo = Dnn.readNetFromDarknet(tinyYoloCfg, tinyYoloWeights);
         }
     }
