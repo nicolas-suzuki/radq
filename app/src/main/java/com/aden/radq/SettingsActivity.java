07b376c5e8a823be2c34a82475dc9622ebbecfa8
Nicolas Suzuki <5541611+nicolas-suzuki@users.noreply.github.com>
2020-09-27 08:01:11 -0300

    Handle Permissions Some fixes Changed error message text for missing permissions in OpenCV Changed layout in settings_activity.xml

--- app/src/main/java/com/aden/radq/SettingsActivity.java
+++ app/src/main/java/com/aden/radq/SettingsActivity.java
@@ -1,24 +1,58 @@
 package com.aden.radq;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 public class SettingsActivity extends AppCompatActivity {
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String SWITCH_CAMERA_FRONT_BACK ="switchCameraFrontBack";
+
+    private SwitchCompat switchCameraFrontBack;
+    private Button bttnSaveSettings;
+
+    private boolean isSwitchBackChecked;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
 
-        Button switchCamera = (Button) findViewById(R.id.bttnSwitchCamera);
-        switchCamera.setOnClickListener(new View.OnClickListener() {
+        switchCameraFrontBack = findViewById(R.id.switchCameraFrontBack);
+        bttnSaveSettings = findViewById(R.id.bttnSaveSettings);
+
+        bttnSaveSettings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //
+                saveData();
             }
         });
+
+        loadData();
+        updateData();
+    }
+
+    public void saveData(){
+        Log.i("settingsData", "saveData()");
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS,MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(SWITCH_CAMERA_FRONT_BACK, switchCameraFrontBack.isChecked());
+        editor.apply();
+    }
+
+    public void loadData(){
+        Log.i("settingsData", "loadData()");
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS,MODE_PRIVATE);
+        isSwitchBackChecked = sharedPreferences.getBoolean(SWITCH_CAMERA_FRONT_BACK,false);
+    }
+
+    public void updateData(){
+        Log.i("settingsData", "updateData()");
+        switchCameraFrontBack.setChecked(isSwitchBackChecked);
     }
 }
